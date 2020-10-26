fcca62aa0c334d9f9eca8323c7d17f228d937ee7
cxumol <cxumol@users.noreply.github.com>
2020-10-26 14:13:44 -0700

    upgrade for all installed python versions
    "Having a lot of python versions on the same machine is quite a common situation." See https://stackoverflow.com/a/52123490

--- pipupgradeall.py
+++ pipupgradeall.py
@@ -1,5 +1,22 @@
 import pkg_resources, subprocess
+import os
+
+def get_all_pythons():
+    '''https://stackoverflow.com/a/52123490'''
+    output, err = subprocess.Popen(
+        ['which', '-a', 'python', 'python3','python2'], 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        ).communicate()
+    return output.decode('utf8').split('\n')[:-1]
+
 def _main():
-    subprocess.run(['pip','install','-U',
-        *[dist.project_name for dist in pkg_resources.working_set]
-    ])
+    
+    all_pythons = []
+    if os.name == 'posix':
+        all_pythons = get_all_pythons()
+    if not all_pythons:
+        all_pythons = ['python']
+    for py in all_pythons:
+        subprocess.run([py, '-m', 'pip','install','-U',
+            *[dist.project_name for dist in pkg_resources.working_set]
+        ])
